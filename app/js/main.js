6f8831f8645aaa90ccc31720a786bf9dcff54524
Roman Nahryshko <nagrishkoo@gmail.com>
2020-10-08 19:58:27 +0300

    Slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,25 @@
-$(function(){
+function createSlick(){  
 
+	$(".slider").not('.slick-initialized').slick({
+		centerMode: true,
+	    // autoplay: true,
+	
+  		slidesToShow: 3,
+	    responsive: [{ 
+	        breakpoint: 768,
+	        settings: {
+	            dots: false,
+	            arrows: false,
+	            infinite: false,
+	            slidesToShow: 1,
+	            slidesToScroll: 1
+	        } 
+	    }]
+	});	
 
+}
 
-  
-});
+createSlick();
+
+//Will not throw error, even if called multiple times.
+$(window).on( 'resize', createSlick );
