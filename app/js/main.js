1ae2e4c2a2070bd05526a9554268a25523ca6eb0
RomanNahryshko <nagrishkoo@gmail.com>
2020-10-09 15:50:04 +0300

    slider + arrows

--- app/js/main.js
+++ app/js/main.js
@@ -2,14 +2,17 @@ function createSlick(){
 
 	$(".slider").not('.slick-initialized').slick({
 		centerMode: true,
-	    // autoplay: true,
+		// autoplay: true,
+		// appendArrows: $('.slick-arrow'),	
+		nextArrow: '<div class="next"><img src="images/Arrow-Right.png" alt=""></div>', 
+		prevArrow: '<div class="prev"><img src="images/Arrow-Left.png" alt=""></div>',
+
 	
   		slidesToShow: 3,
 	    responsive: [{ 
 	        breakpoint: 768,
 	        settings: {
 	            dots: false,
-	            arrows: false,
 	            infinite: false,
 	            slidesToShow: 1,
 	            slidesToScroll: 1
